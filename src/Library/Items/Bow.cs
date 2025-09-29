8522878a93bacbff197990d8a47ffd9059d30f43
paolabelen <paolabelen267@gmail.com>
2025-09-29 11:26:11 -0300

    Bow.cs

--- src/Library/Items/Bow.cs
+++ src/Library/Items/Bow.cs
@@ -1,6 +1,6 @@
 namespace Ucu.Poo.RoleplayGame;
 
-public class  : IItem
+public class Bow: IItem
 {
     public int ValorAtaque
     {
@@ -14,6 +14,13 @@ public class  : IItem
         get
         {
             return 0;
+        }
+    }
+    public string Nombre
+    {
+        get
+        {    
+            return "Arco";
         }
     }
 }
