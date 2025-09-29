5b013f7fcc21e7dc50c34fe7e708d5ff348e0252
paolabelen <paolabelen267@gmail.com>
2025-09-29 11:26:50 -0300

    Shield.cs

--- src/Library/Items/Shield.cs
+++ src/Library/Items/Shield.cs
@@ -16,4 +16,11 @@ public class Shield : IItem
             return 0;
         }
     }
+    public string Nombre
+    {
+        get
+        {    
+            return "Escudo";
+        }
+    }
 }
